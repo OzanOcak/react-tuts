fe5946d7f4d02361940267770eba347184327c16
OzanOcak <ozanocak@gmail.com>
2022-10-11 10:29:15 -0400

    initial commit

--- src/hooks/useInput.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export function useInput(initialState) {
-  const [value, setValue] = useState(initialState);
-  return [
-    { value, onChange: (e) => setValue(e.target.value) },
-    () => setValue(initialState),
-  ];
-}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import React, { createContext, useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
-import App from "./App";
-
-export const GreetingContext = createContext();
-
-const greetings = [
-  { id: "1", greet: "hello" },
-  { id: "2", greet: "bonjour" },
-  { id: "3", greet: "hola" },
-  { id: "4", greet: "hallo" },
-];
-
-export const useGreetings = () => useContext(GreetingContext); // custom hook to import
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <GreetingContext.Provider value={{ greetings }}>
+  <React.StrictMode>
     <App />
-  </GreetingContext.Provider>
+  </React.StrictMode>
 );
